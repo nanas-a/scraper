c2d4f57f9a1bd24988bddcb9262bac777757d602
nasri adzlani <nasriadzlani@live.com>
2024-01-13 17:17:45 +0700

    hehe

--- service/cnbc/url_list.py
+++ service/cnbc/url_list.py
@@ -13,31 +13,36 @@ def start():
     total_crawl = 1
     with client:
         for i in range(1, 50):
-            url = 'https://www.cnbcindonesia.com/market/indeks/5/{}'.format(
-                i)
-            print("Fetching url {}".format(url))
-            page = requests.get(url)
-            if page.status_code != 200:
+            try:
+                url = 'https://www.cnbcindonesia.com/market/indeks/5/{}'.format(
+                    i)
+                print("Fetching url {}".format(url))
+                page = requests.get(url)
+                if page.status_code != 200:
+                    print("Error while fetching url")
+                    break
+                soup = BeautifulSoup(page.text, 'html.parser')
+                media = soup.find('ul', class_='media_rows')
+                list_media = media.find_all('li')
+                for index, val in enumerate(list_media):
+                    link = val.find('a')['href']
+                    if validate_url(link):
+                        detail = scraper(link)
+                        result = {
+                            'link': link,
+                            'source': 'cnbc indonesia',
+                            'scrape_date': time.strftime("%Y-%m-%d %H:%M:%S"),
+                            **detail
+                        }
+                        result['_id'] = str(ObjectId())
+                        collection.insert_one(result)
+                        print(str(total_crawl) + " Success insert " +
+                              result['title']+" to database")
+                        total_crawl += 1
+                    else:
+                        print("Data already exist")
+                    print("=====================================")
+            except Exception as e:
+                print(e)
                 print("Error while fetching url")
-                break
-            soup = BeautifulSoup(page.text, 'html.parser')
-            media = soup.find('ul', class_='media_rows')
-            list_media = media.find_all('li')
-            for index, val in enumerate(list_media):
-                link = val.find('a')['href']
-                if validate_url(link):
-                    detail = scraper(link)
-                    result = {
-                        'link': link,
-                        'source': 'cnbc indonesia',
-                        'scrape_date': time.strftime("%Y-%m-%d %H:%M:%S"),
-                        **detail
-                    }
-                    result['_id'] = str(ObjectId())
-                    collection.insert_one(result)
-                    print(str(total_crawl) + " Success insert " +
-                          result['title']+" to database")
-                    total_crawl += 1
-                else:
-                    print("Data already exist")
-                print("=====================================")
+                continue
--- service/cnn/url_list.py
+++ service/cnn/url_list.py
@@ -14,32 +14,37 @@ def start():
     total_crawl = 1
     with client:
         for i in range(1, 50):
-            url = 'https://www.cnnindonesia.com/ekonomi/indeks/5/{}'.format(
-                i)
-            print("Fetching url {}".format(url))
-            page = requests.get(url)
-            if page.status_code != 200:
+            try:
+                url = 'https://www.cnnindonesia.com/ekonomi/indeks/5/{}'.format(
+                    i)
+                print("Fetching url {}".format(url))
+                page = requests.get(url)
+                if page.status_code != 200:
+                    print("Error while fetching url")
+                    break
+                soup = BeautifulSoup(page.text, 'html.parser')
+                media = soup.find('div', class_='w-leftcontent')
+                for article in media.find_all('article'):
+                    link = article.find('a')['href']
+                    if validate_url(link):
+                        detail = scrape(link)
+                        result = {
+                            'link': link,
+                            'source': 'cnbc indonesia',
+                            'scrape_date': time.strftime("%Y-%m-%d %H:%M:%S"),
+                            **detail
+                        }
+                        result['_id'] = str(ObjectId())
+                        collection.insert_one(result)
+                        print(str(total_crawl)+" Success insert " +
+                              result['title']+" to database")
+                        total_crawl += 1
+                    else:
+                        print("Data already exist")
+                        # exit()
+                    print("=====================================")
+                # exit()
+            except Exception as e:
+                print(e)
                 print("Error while fetching url")
-                break
-            soup = BeautifulSoup(page.text, 'html.parser')
-            media = soup.find('div', class_='w-leftcontent')
-            for article in media.find_all('article'):
-                link = article.find('a')['href']
-                if validate_url(link):
-                    detail = scrape(link)
-                    result = {
-                        'link': link,
-                        'source': 'cnbc indonesia',
-                        'scrape_date': time.strftime("%Y-%m-%d %H:%M:%S"),
-                        **detail
-                    }
-                    result['_id'] = str(ObjectId())
-                    collection.insert_one(result)
-                    print(str(total_crawl)+" Success insert " +
-                          result['title']+" to database")
-                    total_crawl += 1
-                else:
-                    print("Data already exist")
-                    # exit()
-                print("=====================================")
-            # exit()
+                continue
